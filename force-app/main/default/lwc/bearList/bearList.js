5fafe124a46d4450071e1223c7c6d38dc26aebaf
Seng <samol.seng@accenture.com>
2019-04-22 13:45:51 -0400

    update bearList to use @wire instead of imperative apex

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,21 +1,10 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import getAllBears from '@salesforce/apex/BearController.getAllBears';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 
 export default class BearList extends LightningElement {
-  @track bears = {};
-  
+  @wire(getAllBears)bears;
   appResources = {
     bearSilhouette: ursusResources + '/img/standing-bear-silhouette.png'
   };
-
-  connectedCallback() {
-    this.loadBears();
-  }
-
-  loadBears() {
-    getAllBears()
-      .then(result => { this.bears.data = result })
-      .catch(error => { this.bears.error = error });
-  }
 }
