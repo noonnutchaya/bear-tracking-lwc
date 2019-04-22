8d75f74fe00ead94d7536e4d56924cc9b057e70b
Seng <samol.seng@accenture.com>
2019-04-22 14:27:04 -0400

    restyle bearList

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,11 +1,16 @@
 import { LightningElement, track, wire } from 'lwc';
-import searchBears from '@salesforce/apex/BearController.searchBears';
+import { loadStyle } from 'lightning/platformResourceLoader';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
+import searchBears from '@salesforce/apex/BearController.searchBears';
 
 export default class BearList extends LightningElement {
   @track searchTerm = '';
   @wire(searchBears, { searchTerm: '$searchTerm' })bears;
   
+  connectedCallback() {
+    loadStyle(this, ursusResources + '/style.css');
+  }
+
   handleSearchTermChange(event) {
     window.clearTimeout(this.delayTimeout);
     const searchTerm = event.target.value;
