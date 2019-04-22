9a525fb06566deaf3378e5b7b8832e61d3c571fc
Seng <samol.seng@accenture.com>
2019-04-22 14:01:24 -0400

    add search feature to bearList

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,10 +1,20 @@
-import { LightningElement, wire } from 'lwc';
-import getAllBears from '@salesforce/apex/BearController.getAllBears';
+import { LightningElement, track, wire } from 'lwc';
+import searchBears from '@salesforce/apex/BearController.searchBears';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 
 export default class BearList extends LightningElement {
-  @wire(getAllBears)bears;
+  @track searchTerm = '';
+  @wire(searchBears, { searchTerm: '$searchTerm' })bears;
   appResources = {
     bearSilhouette: ursusResources + '/img/standing-bear-silhouette.png'
   };
+  handleSearchTermChange(event) {
+    window.clearTimeout(this.delayTimeout);
+    const searchTerm = event.target.value;
+    // eslint-disable-next-line
+    this.delayTimeout = setTimeout(() => { this.searchTerm = searchTerm }, 300);
+  }
+  get hasResults() {
+    return this.bears.data.length > 0;
+  }
 }
